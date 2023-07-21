9f3eb54296454746c0f934de3c3854ed56e3cc1c
Erik Sandoval <eriksandoval.py@gmail.com>
2023-07-20 22:15:53 -0500

    first commit

---
+++ src/main/java/com/eriksandoval/enduserinteractionsusingtemplateenginesthymeleafdemoproject/EndUserInteractionsUsingTemplateEnginesThymeleafDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.eriksandoval.enduserinteractionsusingtemplateenginesthymeleafdemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndUserInteractionsUsingTemplateEnginesThymeleafDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndUserInteractionsUsingTemplateEnginesThymeleafDemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eriksandoval/enduserinteractionsusingtemplateenginesthymeleafdemoproject/EndUserInteractionsUsingTemplateEnginesThymeleafDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eriksandoval.enduserinteractionsusingtemplateenginesthymeleafdemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndUserInteractionsUsingTemplateEnginesThymeleafDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
